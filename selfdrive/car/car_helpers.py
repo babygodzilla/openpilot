c043c49c1d0755ee4a4d7e5afa9a58e1b389f216
babygodzilla <wleung2@hotmail.com>
2022-04-24 23:24:29 -0400

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -81,8 +81,8 @@ interfaces = load_interfaces(interface_names)
 
 # **** for use live only ****
 def fingerprint(logcan, sendcan):
-  fixed_fingerprint = os.environ.get('FINGERPRINT', "")
-  skip_fw_query = os.environ.get('SKIP_FW_QUERY', False)
+  fixed_fingerprint = "HONDA CR-V 2017" #os.environ.get('FINGERPRINT', "")
+  skip_fw_query = True #os.environ.get('SKIP_FW_QUERY', False)
 
   if not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
